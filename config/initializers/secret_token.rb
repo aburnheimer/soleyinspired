d9546b4921ce29b722b41aca515d9ecf923ec0cd
Andrew Burnheimer <Andrew_Burnheimer@cable.comcast.com>
2011-12-18 20:54:30 -0500

    Actual initial addition of Rails, with a home/index view.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soleyinspired::Application.config.secret_token = 'e5c47a493fe235bcbaa88a8d63016415d94eb98e0703048bc3750abd9817cad8cc607bcda53c91f794dafd310817c50b707a900b7582dd5427686a7172680972'
