0ba2f6c8962982c2c32d63db9790255d28d829fe
Andrew Burnheimer <ABurnheimer@gmail.com>
2012-02-01 02:07:31 -0500

    Add form and pendant info from "updated order form" thread, 01/30/12 10:18AM

--- public/pendants.php
+++ public/pendants.php
@@ -18,7 +18,175 @@
                 <a href="aboutme.html"><img id="bottom_menu_item" src="website-boutton-aboutme.png"></a>
             </div>
             <div id="content">
-				<img id="top_label" src="Soley-Inspired-pendant.png">
+                <img id="top_label" src="Soley-Inspired-pendant.png">
+
+                <h1 id='form_title'>Request a Quote&hellip;</h1>
+<?php
+// Taken from http://www.w3schools.com/php/php_mail.asp
+if (isset($_REQUEST['email']))
+//if "email" is filled out, send email
+  {
+  //send email
+  $fullName = $_REQUEST['fullName'] ;
+  $email = $_REQUEST['email'] ;
+  $phone = $_REQUEST['phone'] ;
+  $coinCountry = $_REQUEST['coinCountry'] ;
+  $coinValu = $_REQUEST['coinValu'] ;
+  $coinAcqu = $_REQUEST['coinAcqu'] ;
+  $settingPref = $_REQUEST['settingPref'] ;
+  $stylePref = $_REQUEST['stylePref'] ;
+  $specialRequ = $_REQUEST['specialRequ'] ;
+
+  $subject = "Quote Request for a " . $settingPref . " " . 
+        $coinCountry . " coin from " . $fullName ;
+  $message = <<<EMESSAGE
+Full Name: $fullName
+
+Email Address: $email
+
+Phone #: $phone
+
+Coin Country of Origin: $coinCountry
+
+Coin Face Value in USD ($): $coinValu
+
+Coin Acquisition: $coinAcqu
+
+Setting Preference: $settingPref
+
+Style Preference: $stylePref
+
+Special Requests: $specialRequ
+EMESSAGE;
+
+  mail("info@soleyinspired.com", $subject, $message, "From:" . $email);
+
+  echo "<p id='art_detail'>Thank you for your request.  We will " .
+    "respond back with more information shortly.</p>";
+  }
+else
+//if "email" is not filled out, display the form
+  {
+  echo <<<FORMML
+                <form method='post' action='pendants.php'>
+
+                    <table id='form_table'>
+                        <tr>
+                            <td id='form_label'><label>Name:</label></td>
+                            <td><input type='text' name='fullName' /></td>
+                            <td id='form_label'><label>Email Address:</label></td>
+                            <td><input type='text' name='email' /></td>
+                        </tr>
+
+                        <tr>
+                            <td id='form_label'><label>Phone &#35;:</label></td>
+                            <td><input type='text' name='phone' /></td>
+                            <td id='form_label'><label>Coin Country of Origin:</label></td>
+                            <td><input type='text' name='coinCountry' /></td>
+                        </tr>
+
+                        <tr>
+                            <td id='form_label'><label>Coin Face Value in USD (&#36;):</label></td>
+                            <td><input type='text' name='coinValu' /></td>
+                            <td id='form_label'><label>Coin Acquisition:</label></td>
+                            <td>
+                                <select name='coinAcqu'>
+                                    <option value='from collection'>Selected from Soley Inspired collection</option>
+                                    <option value='mailed-in'>Mailed in by the customer</option>
+                                    <option value='delivered'>Delivered by the customer</option>
+                                    <option value='to acquire'>Acquired by Soley Inspired for a small fee</option>
+                                </select>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td id='form_label'><label>Setting Preference:</label></td>
+                            <td>
+                                <select name='settingPref'>
+                                    <option value='mounted'>Mounted</option>
+                                    <option value='drilled'>Drilled</option>
+                                    <option value='hand wired'>Hand Wired</option>
+                                    <option value='reversible'>Reversible</option>
+                                    <option value='bezel-set'>Bezel</option>
+                                </select>
+                            </td>
+                            <td id='form_label'><label>Style Preference:</label></td>
+                            <td>
+                                <select name='stylePref'>
+                                    <option value='W011'>Sample W011</option>
+                                    <option value='W012'>Sample W012</option>
+                                    <option value='W013'>Sample W013</option>
+                                    <option value='D014'>Sample D014</option>
+                                    <option value='D011'>Sample D011</option>
+                                    <option value='D012'>Sample D012</option>
+                                    <option value='D013'>Sample D013</option>
+                                    <option value='D014'>Sample D014</option>
+                                    <option value='M011'>Sample M011</option>
+                                    <option value='M012'>Sample M012</option>
+                                    <option value='M013'>Sample M013</option>
+                                    <option value='M014'>Sample M014</option>
+                                    <option value='B011'>Sample B011</option>
+                                    <option value='R011'>Sample R011</option>
+                                    <option value='R012'>Sample R012</option>
+                                    <option value='R013'>Sample R013</option>
+                                    <option value='R014'>Sample R014</option>
+                                </select>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td id='form_label'><label>Special Requests:</label></td>
+                            <td colspan='3'><textarea rows='3' type='text' name='specialRequ'></textarea> </td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td><button type='submit' name='submit'>Submit</button></td>
+                        </tr>
+                    </table>
+                </form>
+FORMML;
+  }
+?>
+
+                <h1 id="form_title">Pricing&hellip;</h1>
+                <table id="info_table">
+                    <tr>
+                        <td>Mounted Coin Pendant with Chain</td>
+                        <td id="info_value">&#36;16.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Drilled Coin Pendant with Chain</td>
+                        <td id="info_value">&#36;18.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Hand Wired Coin Pendant with Chain</td>
+                        <td id="info_value">&#36;22.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Reversible Coin Pendant with Chain</td>
+                        <td id="info_value">&#36;28.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>Bezel-set Coin Pendant with Chain</td>
+                        <td id="info_value">&#36;32.00</td>
+                    </tr>
+                </table>
+                
+                <p id="art_detail">These are average prices and include a 20” chain in a
+                   coordinating finish.  Discounts and/or credits are given to
+                   customers that provide their own coins or forgo the
+                   chain.</p>
+
+               <p id="art_detail">We Accept Cash, Check, Money Order, and Credit Cards (a
+                   Paypal bill will be generated upon request).</p>
+               
+               <p id="art_detail">Worldwide shipping available.  Delivery/Pickup available by
+                   request.</p>
 
                 <table>
                 <tr>
